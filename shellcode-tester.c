ccc7e0dc36eae420b2603e6f3a5061fcb2479a91
jjooa <josemagalhaes6@gmail.com>
2019-10-26 04:29:13 +0100

    Update shellcode-tester.c

--- shellcode-tester.c
+++ shellcode-tester.c
@@ -5,11 +5,11 @@ char shellcode[] = "PLACE SHELLCODE HERE";
 int main()
 {
     int *ret;
-    /* The following instruction sets *ret pointer 
+    /* The following sets *ret pointer 
      * equal to main return address 
      */
     ret = (int *)&ret + 2;
-    /* The following instruction sets the return to the 
+    /* The following sets the return to the 
      * address of shellcode array
      */
     (*ret) = (int)shellcode;
